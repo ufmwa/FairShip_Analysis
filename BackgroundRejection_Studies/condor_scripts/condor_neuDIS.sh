d9247c9cd215ed327ee3e5bbefd719b8bdab9019
anupama-reghunath <anupamareghu@gmail.com>
2025-06-13 08:14:48 +0200

    Add Condor scripts

---
+++ BackgroundRejection_Studies/condor_scripts/condor_neuDIS.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#######################################################################################
+
+#ClusterId=	$1
+#ProcId=$2
+JOB=$3
+
+#######################################################################################
+source /cvmfs/ship.cern.ch/24.10/setUp.sh 
+source /afs/cern.ch/user/a/anupamar/HTCondor/configfiles/config_ECN3_2024.sh #alienv load FairShip/latest-master-release > config_<version>.sh
+echo 'config sourced'
+#######################################################################################
+
+python /afs/cern.ch/user/a/anupamar/FairShip_Analysis/BackgroundRejection_Studies/run_neuDIS.py -i "$JOB"
+
+mkdir -p "/eos/experiment/ship/user/anupamar/BackgroundStudies/neuDIS/$JOB"
+
+OUTPUTDIR="/eos/experiment/ship/user/anupamar/BackgroundStudies/neuDIS/$JOB"
+
+xrdcp selectionparameters_*.root root://eospublic.cern.ch/"$OUTPUTDIR"/
+xrdcp selection_summary_*.csv root://eospublic.cern.ch/"$OUTPUTDIR"/
